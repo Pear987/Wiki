d7ab4949d8dc38a0ea20e84f148c018af9367870
Pear987 <pppear61@gmail.com>
2023-07-17 10:26:07 +0700

    clon for master

---
+++ resources/views/createaccount.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title>Create Account</title>
+</head>
+
+<body>
+    <div class="bg-red-300 h-screen p-10 flex flex-row items-center">
+
+        <div class="bg-blue-400 w-full h-full flex flex-row items-center">
+            <div class="bg-gray-300 flex flex-row items-center">
+                <label class="text-orange-500 font-medium mx-2">
+                    <span>C</span>
+                </label>
+
+                <label class="text-cyan-400 text-[26px] font-medium mx-2 ">
+                    <span>r</span>
+                </label>
+
+            </div>
+
+        </div>
+
+
+    </div>
+
+</body>
+
+</html>
